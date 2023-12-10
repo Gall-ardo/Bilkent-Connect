6b030de2fc12f0b1d220213d3099d0d54cede387
Halil Arda Özongun <halilardaozongun@gmail.com>
2023-12-10 15:01:31 +0300

    permission for gallery added. some share post page's methods added

--- app/src/main/java/com/hao/bilkentconnect/SharePostsActivity.java
+++ app/src/main/java/com/hao/bilkentconnect/SharePostsActivity.java
@@ -1,15 +1,39 @@
 package com.hao.bilkentconnect;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.hao.bilkentconnect.databinding.ActivityMainBinding;
 import com.hao.bilkentconnect.databinding.ActivitySharePostsBinding;
 
+import java.io.IOException;
+
 public class SharePostsActivity extends AppCompatActivity {
     private ActivitySharePostsBinding binding;
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    Uri imageData;
+    Bitmap selectedImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +41,94 @@ public class SharePostsActivity extends AppCompatActivity {
         binding = ActivitySharePostsBinding.inflate(getLayoutInflater());
         View viewRoot = binding.getRoot();
         setContentView(viewRoot);
+        registerLauncher();
+
+
+    }
+
+    void uploadImage(View view) {
+        // check permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view,"Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                       // ask permission
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    }
+                }).show();
+            }
+            else {
+                // ask permission
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+        else {
+            // permission already granted
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallery);
+        }
+    }
+    void sharePost(View view) {
+       // upload image to firebase
+        // download url
+        // save post to firebase
+        // go to main activity
+
+    }
+
+    private void registerLauncher() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode()==RESULT_OK) {
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null) {
+                        // image selected
+                        imageData = intentFromResult.getData();
+                        binding.SelectImage.setImageURI(imageData);
+
+
+
+                        /*
+                        try{
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                ImageDecoder.Source source = ImageDecoder.createSource(SharePostsActivity.this.getContentResolver(), imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.SelectImage.setImageBitmap(selectedImage);
+
+                            }
+                            else {
+
+                                selectedImage = MediaStore.Images.Media.getBitmap(SharePostsActivity.this.getContentResolver(), imageData);
+                                binding.SelectImage.setImageBitmap(selectedImage);
+                            }
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        */
+
+                    }
+                }
+
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result) {
+                    // permission granted
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+                }
+                else {
+                    // permission denied
+                    Toast.makeText(SharePostsActivity.this,"Permission needed for gallery", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 }
