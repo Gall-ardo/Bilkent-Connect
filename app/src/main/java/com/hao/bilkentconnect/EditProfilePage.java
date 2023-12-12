31d7d7aa0bf22fd0d7bfc7f3644eb435c63f711c
Halil Arda Özongun <halilardaozongun@gmail.com>
2023-12-12 16:49:11 +0300

    edit profile finished.

--- app/src/main/java/com/hao/bilkentconnect/EditProfilePage.java
+++ app/src/main/java/com/hao/bilkentconnect/EditProfilePage.java
@@ -1,16 +1,58 @@
 package com.hao.bilkentconnect;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
-import com.hao.bilkentconnect.databinding.ActivityChatBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.hao.bilkentconnect.ModelClasses.Post;
 import com.hao.bilkentconnect.databinding.ActivityEditProfilePageBinding;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class EditProfilePage extends AppCompatActivity {
     private ActivityEditProfilePageBinding binding;
+    String newUserName;
+    String newDescription;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    Uri imageData;
+    Bitmap selectedImage;
+    boolean ppUpdated = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +60,171 @@ public class EditProfilePage extends AppCompatActivity {
         binding = ActivityEditProfilePageBinding.inflate(getLayoutInflater());
         View viewRoot = binding.getRoot();
         setContentView(viewRoot);
+        registerLauncher();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
     }
 
-    public void goBackToProfilePage_2(View view){
+    public void goToProfilePage(View view){
         Intent intent = new Intent(this, ProfilePage.class);
         startActivity(intent);
+        finish();
+    }
+
+    public void saveChanges(View view){
+        newUserName = binding.newUsernameText.getText().toString();
+        newDescription = binding.bioText.getText().toString();
+        Map<String, Object> updates = new HashMap<>();
+
+
+        if(ppUpdated){
+            UUID uuid = UUID.randomUUID();
+            final String imageName = "images/" + uuid + ".jpg";
+            storageReference.child(imageName).putFile(imageData).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //Download URL
+
+                    StorageReference newReference = FirebaseStorage.getInstance().getReference(imageName);
+                    newReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        public void onSuccess(Uri uri) {
+                            String downloadUrl = uri.toString();
+                            updates.put("profilePhoto", downloadUrl);
+
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            String userId = firebaseUser.getUid(); // Get the user's ID
+                            firebaseFirestore.collection("Users").document(userId).update(updates)
+                                    .addOnSuccessListener(aVoid -> Toast.makeText(EditProfilePage.this, "Profile updated successfully", Toast.LENGTH_SHORT).show())
+                                    .addOnFailureListener(e -> Toast.makeText(EditProfilePage.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show());
+
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(EditProfilePage.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+
+        if(newDescription == null|| newUserName == null){
+            Toast.makeText(this,"Both empty",Toast.LENGTH_LONG).show();
+        }else {
+            if (!newUserName.isEmpty()) {
+                updates.put("username", newUserName);
+            }
+            if (!newDescription.isEmpty()) {
+                updates.put("biography", newDescription);
+            }
+        }
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        String userId = firebaseUser.getUid(); // Get the user's ID
+        firebaseFirestore.collection("Users").document(userId).update(updates);
+
+    }
+
+
+
+    public void selectPP(View view) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
+                // ask permission
+                if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_MEDIA_IMAGES)){
+                    Snackbar.make(view,"You have to give permission if you want to share stuff",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission.", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // ask permission
+                            permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES);
+                        }
+                    }).show();
+                }else{
+                    // ask permission
+                    permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES);
+                }
+
+            }else{
+                // already have permission
+                Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                activityResultLauncher.launch(intentToGallery);
+            }
+        }else{
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                // ask permission
+                if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    Snackbar.make(view,"You have to give permission if you want to share stuff",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission.", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // ask permission
+                            permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+                        }
+                    }).show();
+                }else{
+                    // ask permission
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                }
+
+            }else{
+                // already have permission
+                Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                activityResultLauncher.launch(intentToGallery);
+            }
+        }
+        ppUpdated = true;
+    }
+    private void registerLauncher() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode()==RESULT_OK) {
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null) {
+                        // image selected
+                        imageData = intentFromResult.getData();
+                        binding.profilePicture.setImageURI(imageData);
+
+
+                        try{
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                ImageDecoder.Source source = ImageDecoder.createSource(EditProfilePage.this.getContentResolver(), imageData);
+                                selectedImage = ImageDecoder.decodeBitmap(source);
+                                binding.profilePicture.setImageBitmap(selectedImage);
+
+                            }
+                            else {
+
+                                selectedImage = MediaStore.Images.Media.getBitmap(EditProfilePage.this.getContentResolver(), imageData);
+                                binding.profilePicture.setImageBitmap(selectedImage);
+                            }
+                        }
+                        catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }
+
+            }
+        });
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if (result) {
+                    // permission granted
+                    Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intentToGallery);
+                }
+                else {
+                    // permission denied
+                    Toast.makeText(EditProfilePage.this,"Permission needed for gallery", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
