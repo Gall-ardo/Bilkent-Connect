8e3f397ea11b27e1c821c7dd1be0140df1e55b60
Başar Çubukçuoğlu <basarcubukcuoglu@gmail.com>
2023-12-13 15:52:40 +0300

    own post saved post xml added

--- app/src/main/java/com/hao/bilkentconnect/SavedPostPages.java
+++ app/src/main/java/com/hao/bilkentconnect/SavedPostPages.java
@@ -1,14 +1,35 @@
 package com.hao.bilkentconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.hao.bilkentconnect.Adapter.ChatAdapter;
+import com.hao.bilkentconnect.Adapter.PostAdapter;
+import com.hao.bilkentconnect.ModelClasses.Post;
+import com.hao.bilkentconnect.databinding.ActivityChatBinding;
+import com.hao.bilkentconnect.databinding.ActivitySavedPostPagesBinding;
+
+import java.util.ArrayList;
 
 public class SavedPostPages extends AppCompatActivity {
 
+    private PostAdapter postAdapter;
+    private ArrayList<Post> postArrayList;
+    private ActivitySavedPostPagesBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saved_post_pages);
+        binding = ActivitySavedPostPagesBinding.inflate(getLayoutInflater());
+        View viewRoot = binding.getRoot();
+        setContentView(viewRoot);
+
+        postArrayList = new ArrayList<>();
+
+        binding.savedPostsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        postAdapter = new PostAdapter(postArrayList);
+        binding.savedPostsRecyclerView.setAdapter(postAdapter);
     }
 }
