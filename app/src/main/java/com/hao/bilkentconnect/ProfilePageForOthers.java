7b09d78986b776b0b2ccaac2e66b0424ad913414
Halil Arda Özongun <halilardaozongun@gmail.com>
2023-12-14 15:46:49 +0300

    Checkpoint1

--- app/src/main/java/com/hao/bilkentconnect/ProfilePageForOthers.java
+++ app/src/main/java/com/hao/bilkentconnect/ProfilePageForOthers.java
@@ -3,12 +3,19 @@ package com.hao.bilkentconnect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.hao.bilkentconnect.databinding.ActivityProfilePageForOthersBinding;
 
 public class ProfilePageForOthers extends AppCompatActivity {
+
+    private ActivityProfilePageForOthersBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page_for_others);
+        binding = ActivityProfilePageForOthersBinding.inflate(getLayoutInflater());
+        View viewRoot = binding.getRoot();
+        setContentView(viewRoot);
     }
 }
