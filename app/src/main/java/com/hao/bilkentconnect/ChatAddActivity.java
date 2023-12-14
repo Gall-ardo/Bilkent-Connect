a991884dc65c63a0326ab2a6d8032bdfafb3886d
Başar Çubukçuoğlu <basarcubukcuoglu@gmail.com>
2023-12-14 14:17:35 +0300

    chat add & meal comments xmls finished

---
+++ app/src/main/java/com/hao/bilkentconnect/ChatAddActivity.java
@@ -0,0 +1,39 @@
+package com.hao.bilkentconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.hao.bilkentconnect.Adapter.ChatAdapter;
+import com.hao.bilkentconnect.ModelClasses.Chat;
+import com.hao.bilkentconnect.databinding.ActivityChatAddBinding;
+import com.hao.bilkentconnect.databinding.ActivityChatBinding;
+
+import java.util.ArrayList;
+
+public class ChatAddActivity extends AppCompatActivity {
+
+    private ChatAdapter chatAdapter;
+    private ArrayList<Chat> chatArrayList;
+    private ActivityChatAddBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatAddBinding.inflate(getLayoutInflater());
+        View viewRoot = binding.getRoot();
+        setContentView(viewRoot);
+
+        chatArrayList = new ArrayList<>();
+        chatArrayList.add(new Chat(22203359, 2000000));
+        chatArrayList.add(new Chat(22203359, 2000000));
+        chatArrayList.add(new Chat(22203359, 2000000));
+        chatArrayList.add(new Chat(22203359, 2000000));
+
+
+        binding.recyclerAddChatView.setLayoutManager(new LinearLayoutManager(this));
+        chatAdapter = new ChatAdapter(chatArrayList);
+        binding.recyclerAddChatView.setAdapter(chatAdapter);
+    }
+}
--- app/src/main/java/com/hao/bilkentconnect/MealCommentActivity.java
+++ app/src/main/java/com/hao/bilkentconnect/MealCommentActivity.java
@@ -1,15 +1,22 @@
 package com.hao.bilkentconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.hao.bilkentconnect.Adapter.CommentAdapter;
+import com.hao.bilkentconnect.ModelClasses.Comment;
 import com.hao.bilkentconnect.databinding.ActivityMainBinding;
 import com.hao.bilkentconnect.databinding.ActivityMealCommentBinding;
 
+import java.util.ArrayList;
+
 public class MealCommentActivity extends AppCompatActivity {
     private ActivityMealCommentBinding binding;
+    private ArrayList<Comment> commentArrayList;
+    private CommentAdapter commentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,11 @@ public class MealCommentActivity extends AppCompatActivity {
         binding = ActivityMealCommentBinding.inflate(getLayoutInflater());
         View viewRoot = binding.getRoot();
         setContentView(viewRoot);
+
+        commentArrayList = new ArrayList<>();
+
+        binding.mealCommentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        commentAdapter = new CommentAdapter(commentArrayList);
+        binding.mealCommentRecyclerView.setAdapter(commentAdapter);
     }
 }
