adce8b2ae4b89cd03e8c2f14775f654434c8f305
Yunus <emre.erkan@ug.bilkent.edu.tr>
2023-12-15 21:01:51 +0300

    Profile photo added to non-anonymous post view

--- app/src/main/java/com/hao/bilkentconnect/PostView.java
+++ app/src/main/java/com/hao/bilkentconnect/PostView.java
@@ -96,6 +96,7 @@ public class PostView extends AppCompatActivity {
                                         }
                                         else {
                                             binding.topUsernameText.setText(user.getUsername());
+                                            Picasso.get().load(user.getProfilePhoto()).into(binding.profilePicture);
                                         }
                                     }
                                 }
