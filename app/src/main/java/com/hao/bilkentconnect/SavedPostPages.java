ee565fb2e4638d832759a71f442103abead6a832
Halil Arda Özongun <halilardaozongun@gmail.com>
2023-12-19 12:37:04 +0300

    liked posts added.

--- app/src/main/java/com/hao/bilkentconnect/SavedPostPages.java
+++ app/src/main/java/com/hao/bilkentconnect/SavedPostPages.java
@@ -6,10 +6,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.hao.bilkentconnect.Adapter.ChatAdapter;
 import com.hao.bilkentconnect.Adapter.PostAdapter;
 import com.hao.bilkentconnect.ModelClasses.Post;
+import com.hao.bilkentconnect.ModelClasses.User;
 import com.hao.bilkentconnect.databinding.ActivityChatBinding;
 import com.hao.bilkentconnect.databinding.ActivitySavedPostPagesBinding;
 
@@ -20,6 +25,8 @@ public class SavedPostPages extends AppCompatActivity implements OnPostClickList
     private PostAdapter postAdapter;
     private ArrayList<Post> postArrayList;
     private ActivitySavedPostPagesBinding binding;
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +34,37 @@ public class SavedPostPages extends AppCompatActivity implements OnPostClickList
         View viewRoot = binding.getRoot();
         setContentView(viewRoot);
 
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         postArrayList = new ArrayList<>();
-
-        binding.savedPostsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         postAdapter = new PostAdapter(postArrayList, this);
+        binding.savedPostsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         binding.savedPostsRecyclerView.setAdapter(postAdapter);
+
+        loadLikedPosts();
+    }
+
+    private void loadLikedPosts() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            return;
+        }
+
+        String userId = currentUser.getUid();
+        db.collection("Users").document(userId).get().addOnSuccessListener(documentSnapshot -> {
+            User user = documentSnapshot.toObject(User.class);
+            if (user != null && user.getLikedPosts() != null) {
+                for (String postId : user.getLikedPosts()) {
+                    db.collection("Posts").document(postId).get().addOnSuccessListener(postSnapshot -> {
+                        Post post = postSnapshot.toObject(Post.class);
+                        if (post != null) {
+                            postArrayList.add(post);
+                            postAdapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        }).addOnFailureListener(e -> Toast.makeText(SavedPostPages.this, "Error loading liked posts", Toast.LENGTH_SHORT).show());
     }
 
     @Override
