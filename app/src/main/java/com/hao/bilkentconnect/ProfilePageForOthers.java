779ebf7128fea2ab7301b2f535851e7fbd7277fd
Halil Arda Özongun <halilardaozongun@gmail.com>
2023-12-20 11:55:18 +0300

    email added

--- app/src/main/java/com/hao/bilkentconnect/ProfilePageForOthers.java
+++ app/src/main/java/com/hao/bilkentconnect/ProfilePageForOthers.java
@@ -6,13 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.hao.bilkentconnect.ModelClasses.Chat;
+import com.hao.bilkentconnect.ModelClasses.User;
 import com.hao.bilkentconnect.databinding.ActivityProfilePageForOthersBinding;
 import com.squareup.picasso.Picasso;
 
@@ -26,6 +29,7 @@ public class ProfilePageForOthers extends AppCompatActivity {
     FirebaseStorage db;
     FirebaseAuth firebaseAuth;
     String curr_user;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +42,7 @@ public class ProfilePageForOthers extends AppCompatActivity {
         curr_user = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         Intent intent = getIntent();
-        String userId = intent.getStringExtra("userId");
+        userId = intent.getStringExtra("userId");
         String username = intent.getStringExtra("username");
         String profilePhoto = intent.getStringExtra("profilePhoto");
         String biography = intent.getStringExtra("biography");
@@ -55,33 +59,59 @@ public class ProfilePageForOthers extends AppCompatActivity {
         backButtonImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();  // Finish the activity when the back button is clicked
+                finish();
             }
         });
 
     }
 
-
-
-    public void createChat(){
-        Chat chat = new Chat(curr_user,"sSDo3MvvJxfLkSoO1vYw4SYZXsF3");
-        HashMap<String, Object> chatData = new HashMap<>();
-        chatData.put("user1ID", curr_user);
-        chatData.put("user2ID", "sSDo3MvvJxfLkSoO1vYw4SYZXsF3");
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-
-    }
     public void addFriendProfilePageForOthers(View view) {
-        // add user to friend list
+        if (userId == null) {
+            Toast.makeText(this, "Profile user ID is not available.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-
+        db.collection("Users").document(currentUserId)
+                .update("friendIds", FieldValue.arrayUnion(userId))
+                .addOnSuccessListener(aVoid -> Toast.makeText(this, "Friend added successfully!", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(this, "Failed to add friend: " + e.getMessage(), Toast.LENGTH_SHORT).show());
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         finish();
     }
 
+    public void goBacktoPostView(View view) {
+        finish();
+    }
+    public void goToChatPage(View view) {
+        if (userId == null) {
+            Toast.makeText(this, "User ID is not available.", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-    
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Users").document(userId)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        User user = documentSnapshot.toObject(User.class);
+                        if (user != null) {
+                            String email = user.getEmail();
+                            Toast.makeText(this, "Email: " + email, Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(this, "User not found.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(e -> Toast.makeText(this, "Error fetching user details: " + e.getMessage(), Toast.LENGTH_LONG).show());
+    }
+
+
+
+
 }
